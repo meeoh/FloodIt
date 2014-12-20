0fb3885a6d04e542d33ed95336d44739eacd9b32
meeoh <shameel.abdullah@gmail.com>
2014-12-20 00:48:16 -0500

    Cell Implementation. Added switch Colour, getters, and notify

--- FloodIt/cell.cc
+++ FloodIt/cell.cc
@@ -6,17 +6,20 @@
 using namespace std;
 
 //Cell constructor
-Cell::Cell(int x, int y):x(x),y(y){
+Cell::Cell(int x, int y):x(x),y(y),neighbours(4,NULL){
 
 	//Generate random colour
 	int colourChoice = (rand()%3) + 1;	
+	colour = 'B';
 	//Assign colour
+	/*
 	switch (colourChoice){		
 	case 1:		colour = 'R';	break;
 	case 2:		colour = 'B';	break;
 	case 3:		colour = 'G';	break;
 	case 4:		colour = 'Y';	break;
-	}
+	
+	}*/
 }
 
 //getColour()
@@ -25,11 +28,29 @@ char Cell :: getColour(){
 	return colour;
 }
 
+
+
 //notify()
-//notifies all neighbours of the current cell
-void Cell::notify(){
+void Cell::notify(char oldColour,char newColour){
+	cout << "old colour : " << oldColour << " newColour : " << newColour << endl;
+	if (oldColour == colour){			
+		this->switchColour(oldColour);		
+	}	
+}
+
+void Cell::switchColour(char newColour) {
+	char oldColour = colour;
+	//cout << "old colour : " << oldColour << " newColour : " << newColour << endl;
+	if (oldColour == newColour){ return; }	
+	colour = newColour;	
+
 	//Loop through all neighbours, and call notify on each one
-	for(int i = 0; i < numNeighbours; i++){
-	neighbours[i]->notify();
+	for (int i = 0; i < numNeighbours; i++){
+		neighbours[i]->notify(oldColour, newColour);
 	}
 }
+
+
+int Cell:: getX(){ return x; }
+int Cell::getY(){ return y; }
+
