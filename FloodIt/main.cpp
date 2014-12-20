abea93528c282688cbe661939ae7013a2c54bc57
meeoh <shameel.abdullah@gmail.com>
2014-12-19 21:37:38 -0500

    Added random generation seed

--- FloodIt/main.cpp
+++ FloodIt/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include "cell.h"
 #include "board.h"
+#include <time.h>
 
 using namespace std;
 
 int main(){
+	srand(time(NULL));
+
 	cout << "Hello World!" << endl;
 	Board *board = new Board(5, 10);
 	cout << *board;
 
 
 
-
+	delete board;
 	getchar();
 }
