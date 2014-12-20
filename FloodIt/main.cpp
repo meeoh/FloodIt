27f7977e46bb9b7d4617d3ae6bf84a77eca1daee
meeoh <shameel.abdullah@gmail.com>
2014-12-20 00:48:52 -0500

    Updated main

--- FloodIt/main.cpp
+++ FloodIt/main.cpp
@@ -9,9 +9,14 @@ int main(){
 	srand(time(NULL));
 
 	cout << "Hello World!" << endl;
-	Board *board = new Board(5, 10);
+	Board *board = new Board(5, 5);
 	cout << *board;
 
+	Cell *origin = board->getCell(0, 0);
+
+	origin->switchColour('R');
+
+	cout << *board;
 
 
 	delete board;
