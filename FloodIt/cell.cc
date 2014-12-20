f4c3b57b20c1e0610ab7e8fb7df962aea01b507d
meeoh <shameel.abdullah@gmail.com>
2014-12-19 21:36:52 -0500

    Updated Cell implementation. Added random generation for colour of cells. Also added comments

--- FloodIt/cell.cc
+++ FloodIt/cell.cc
@@ -1,21 +1,34 @@
 #include "cell.h"
+#include <cstdlib>
+#include <time.h>
 #include <iostream>
 
 using namespace std;
 
+//Cell constructor
 Cell::Cell(int x, int y):x(x),y(y){
-	colour = 'R';
+
+	//Generate random colour
+	int colourChoice = (rand()%3) + 1;	
+	//Assign colour
+	switch (colourChoice){		
+	case 1:		colour = 'R';	break;
+	case 2:		colour = 'B';	break;
+	case 3:		colour = 'G';	break;
+	case 4:		colour = 'Y';	break;
+	}
 }
 
-string Cell :: getColour(){
+//getColour()
+//Returns colour of the current cell
+char Cell :: getColour(){
 	return colour;
 }
 
-
+//notify()
+//notifies all neighbours of the current cell
 void Cell::notify(){
-	#ifdef DEBUG 
-	cout << "notify called" << endl;
-	#endif
+	//Loop through all neighbours, and call notify on each one
 	for(int i = 0; i < numNeighbours; i++){
 	neighbours[i]->notify();
 	}
