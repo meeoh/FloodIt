8e881a7e2d38e4d56799d518c279651f1f20fbf2
meeoh <shameel.abdullah@gmail.com>
2014-12-19 21:11:23 -0500

    Updated Cell Header. To hold notify, colour, neighbours.

--- FloodIt/cell.h
+++ FloodIt/cell.h
@@ -1,15 +1,19 @@
 #ifndef _CELL_
 #define _CELL_
 
+#include <iostream>
+
 class Cell {
 
 	int numNeighbours;
 	Cell* neighbours[4];
 	int x,y;
+	char colour;
 
 public:
 	void notify();
-	Cell();
+	Cell(int x, int y);
+	std::string getColour();
 };
 
 #endif
