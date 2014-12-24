cb4818575f3c0a9a29d3269cc278d3ae88c24130
meeoh <shameel2000@hotmail.com>
2014-12-24 03:18:58 -0500

    Added comments. Added neighbour cells. Added destructor. Added addColour and hasWon methods.

--- FloodIt/board.cc
+++ FloodIt/board.cc
@@ -7,28 +7,63 @@ using namespace std;
 
 
 //Board constructor
-Board::Board(int sizeX, int sizeY):board(sizeX, vector< Cell* >(sizeY)), sizeX(sizeX), sizeY(sizeY) {
-	cout << "board constructor called" << endl;
+Board::Board(int sizeX, int sizeY):board(sizeX, vector< Cell* >(sizeY)), sizeX(sizeX), sizeY(sizeY) {	
+	redCount = 0;
+	blueCount = 0;
+	yellowCount = 0;
+	greenCount = 0;
+
 	//Populate 2D Array with cells
 	for (int i = 0; i < sizeY; i++){
 		for (int j = 0; j < sizeX; j++){
-			board[j][i] = new Cell(j, i);
+			//Pass the cell a pointer to the board
+			board[j][i] = new Cell(j, i,this);
+			//Keep track of the colours of each cell, to tell if a player has won
+			//If the cell is red
+			if (board[j][i]->getColour() == 'R'){
+				redCount++;
+			}
+			//If the cell is blue
+			else if (board[j][i]->getColour() == 'B'){
+				blueCount++;
+			}
+			//If the cell is green
+			else if (board[j][i]->getColour() == 'G'){
+				greenCount++;
+			}
+			//Otherwise the cell is yellow
+			else {
+				yellowCount++;
+			}
 		}		
 	}
 
+	//Loop through the 2D Array and assign each cell its neighbours
 	for (int i = 0; i < sizeY; i++){
 		for (int j = 0; j < sizeX; j++){
-			//board[j][i]->
+			//If the cell to the right of the current cell exists, assign it as a neighbour
+			if (j + 1 < sizeX){								
+				board[j][i]->setNeighbour(board[j+1][i]);
+			}
+			//If the cell to the left of the current cell exists, assign it as a neighbour
+			if (j - 1 >= 0){				
+				board[j][i]->setNeighbour(board[j - 1][i]);
+			}
+			//If the cell to the north of the current cell exists, assign it as a neighbour
+			if (i + 1 < sizeY){				
+				board[j][i]->setNeighbour(board[j][i + 1]);
+			}
+			//If the cell to the south of the current cell exists, assign it as a neighbour
+			if (i - 1 >= 0){				
+				board[j][i]->setNeighbour(board[j][i - 1]);
+			}
 		}
 	}
-	
-	//set neighbours
-	
+
 }
 //operator<<
 //overload output operator to print grid correctly
-std::ostream& operator<<(std::ostream &out, const Board &b){
-	cout << "board operator<< called" << endl;
+std::ostream& operator<<(std::ostream &out, const Board &b){	
 	for (int i = 0; i < b.sizeY; i++){
 		for (int j = 0; j < b.sizeX; j++){
 			out << b.board[j][i]->getColour();
@@ -38,6 +73,33 @@ std::ostream& operator<<(std::ostream &out, const Board &b){
 	return out;
 }
 
+//getCell(int x, int y)
+//returns a cell on the board at (x,y)
 Cell* Board::getCell(int x, int y){
 	return board[x][y];
+}
+
+//Board destructor
+Board::~Board(){
+	for (int i = 0; i < sizeY; i++){
+		for (int j = 0; j < sizeX; j++){
+			delete board[j][i];
+		}
+	}
+}
+
+//addColour (char colour)
+//adds one to the count of the passed in 'colour'
+void Board::addColour(char colour){
+	if (colour == 'R'){ redCount++; }
+	else if (colour == 'B'){ blueCount++; }
+	else if (colour == 'G'){ greenCount++; }
+	else { yellowCount++; }
+}
+
+//hasWon()
+//Check if a player has won
+bool Board::hasWon(){
+	if (redCount == sizeX * sizeY || blueCount == sizeX * sizeY || greenCount == sizeX * sizeY || yellowCount == sizeX * sizeY){ return true; }
+	return false;
 }
