d8e5e5860daff58ee0555f219a020fa355b602d4
meeoh <shameel2000@hotmail.com>
2014-12-24 03:20:30 -0500

    Modified main text file to play. Switching doesnt work properly at the moment

--- FloodIt/main.cpp
+++ FloodIt/main.cpp
@@ -2,23 +2,41 @@
 #include "cell.h"
 #include "board.h"
 #include <time.h>
+#include <limits>
 
 using namespace std;
 
 int main(){
 	srand(time(NULL));
 
-	cout << "Hello World!" << endl;
-	Board *board = new Board(5, 5);
-	cout << *board;
+	int sizeX=0, sizeY=0;	
+	
+	//greeting
+	cout << "Hello, Welcome to Shameels FloodIt 1.0!" << endl;
+	cout << "To get started, enter the x dimension of the game board you wish to play on: ";
+	//Dimension input
+	cin >> sizeX;
+	cout << "Now enter a y dimension: ";
+	cin >> sizeY;
 
+	//Create board and assign origin cell
+	Board *board = new Board(sizeX, sizeY);
 	Cell *origin = board->getCell(0, 0);
-
-	origin->switchColour('R');
-
+	//Display initial board
 	cout << *board;
 
-
+	//get input from user
+	while (true){
+		char choice;
+		cout << "What colour would you like to change the origin (top left corner) to (R,B,G,Y)?" << endl;
+		cin >> choice;
+		//uppercase their choice
+		origin->switchColour(choice);
+		cout << *board;
+		if (board->hasWon()){ cout << "YOU WIN"; break; }
+		
+	}
+	getchar();
 	delete board;
 	getchar();
 }
